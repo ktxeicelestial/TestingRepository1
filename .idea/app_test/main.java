1ddbab6b7f725bbcc2e4109690cbabec4ae7b4d1
4510158 <jahquan.williams@bwtek.com>
2023-12-12 09:16:17 -0500

    first time commit
    Signed-off-by: 4510158 <jahquan.williams@bwtek.com>

---
+++ .idea/app_test/main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.nio.ByteBuffer;
+
+public class MainProject {
+    public static void main(String args) {
+        System.out.println("Testing program output");
+        Log.w("", "");
+
+    }
+
+}
